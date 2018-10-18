df456e2e8c82fd332b03cbd7d0e75c45e7321709
sergio.herrera2 <sergio.herrera2@alu.uclm.es>
2018-10-18 11:20:32 +0200

    Initial commit

---
+++ src/main/java/es/thespeciallabs/iso2/restaurant_chain/App.java
@@ -0,0 +1,13 @@
+package es.thespeciallabs.iso2.restaurant_chain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
