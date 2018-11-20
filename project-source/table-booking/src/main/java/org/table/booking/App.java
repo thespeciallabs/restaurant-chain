5c8f6f9c0a96e618e85ac893f4975797d8e922fd
sergio.herrera2 <sergio.herrera2@alu.uclm.es>
2018-11-20 20:35:32 +0100

    Fix number of version in i2

--- project-source/table-booking/src/main/java/org/table/booking/App.java
+++ project-source/table-booking/src/main/java/org/table/booking/App.java
@@ -5,7 +5,7 @@ import org.table.booking.presentation.IU_TableBooking;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("-----\tRestaurant Chain Manager.\t(v0.0.2)\t-----\n");
+		System.out.println("-----\tRestaurant Chain Manager.\t(v0.3)\t-----\n");
 		System.out.println("-----\tTABLE BOOKING\t-----");
 		IU_Login.main(null);
 		IU_TableBooking.main(null);
