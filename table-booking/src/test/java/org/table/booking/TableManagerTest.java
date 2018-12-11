d0975df6d760f2792716a5fe1ba7ed874e03f935
sergio.herrera2 <sergio.herrera2@alu.uclm.es>
2018-12-11 19:41:24 +0100

    First steps in JUnit tests

---
+++ table-booking/src/test/java/org/table/booking/TableManagerTest.java
@@ -0,0 +1,27 @@
+package org.table.booking;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.table.booking.domain.Table;
+import org.table.booking.domain.TableManager;
+
+public class TableManagerTest {
+
+	private Table t;
+
+	@Before
+	public void before() {
+		Table t = new Table();
+	}
+
+	@Test(timeout = 1000)
+	public void make_reservationTest() {
+
+		Table expected = null;
+		t = TableManager.make_reservation(1, 9);
+		assertEquals(expected, t);
+	}
+
+}
