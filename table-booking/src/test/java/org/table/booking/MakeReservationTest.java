538ba88117df6fa6042b9d1b58fa60cd4fb4c1cc
sergio.herrera2 <sergio.herrera2@alu.uclm.es>
2018-12-13 20:23:18 +0100

    MakeReservation tests added

--- table-booking/src/test/java/org/table/booking/MakeReservationTest.java
+++ table-booking/src/test/java/org/table/booking/MakeReservationTest.java
@@ -20,84 +20,95 @@ public class MakeReservationTest {
 	public void makeReservationTC1() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", -2);
+		t = TableManager.make_reservation(-5, "-2:00", -2);
 		assertEquals(expected.state(), t.state());
 	}
 
+	@Test // (timeout = 1000) @Skip
 	public void makeReservationTC2() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", 2);
+		t = TableManager.make_reservation(2, "11:00", 3);
 		assertEquals(expected.state(), t.state());
 	}
 
+	@Test // (timeout = 1000) @Skip
 	public void makeReservationTC3() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", 2);
+		t = TableManager.make_reservation(4, "13:30", 7);
 		assertEquals(expected.state(), t.state());
 	}
 
+	@Test // (timeout = 1000) @Skip
 	public void makeReservationTC4() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", 2);
+		t = TableManager.make_reservation(0, "16:45", 0);
 		assertEquals(expected.state(), t.state());
 	}
 
+	@Test // (timeout = 1000) @Skip
 	public void makeReservationTC5() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", 2);
+		t = TableManager.make_reservation(3, "22:15", 6);
 		assertEquals(expected.state(), t.state());
 	}
 
+	@Test // (timeout = 1000) @Skip
 	public void makeReservationTC6() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", 2);
+		t = TableManager.make_reservation(2000, "25:30", 2000);
 		assertEquals(expected.state(), t.state());
 	}
 
+	@Test // (timeout = 1000) @Skip
 	public void makeReservationTC7() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", 2);
+		t = TableManager.make_reservation(-5, "9:00", 2);
 		assertEquals(expected.state(), t.state());
 	}
 
+	@Test // (timeout = 1000) @Skip
 	public void makeReservationTC8() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", 2);
+		t = TableManager.make_reservation(2, "13:30", 3);
 		assertEquals(expected.state(), t.state());
 	}
 
+	@Test // (timeout = 1000) @Skip
 	public void makeReservationTC9() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", 2);
+		t = TableManager.make_reservation(4, "15:00", 7);
 		assertEquals(expected.state(), t.state());
 	}
 
+	@Test // (timeout = 1000) @Skip
 	public void makeReservationTC10() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", 2);
+		t = TableManager.make_reservation(0, "20:45", 0);
 		assertEquals(expected.state(), t.state());
 	}
 
+	@Test // (timeout = 1000) @Skip
 	public void makeReservationTC11() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", 2);
+		t = TableManager.make_reservation(3, "23:00", 6);
 		assertEquals(expected.state(), t.state());
 	}
 
+	@Test // (timeout = 1000) @Skip
 	public void makeReservationTC12() {
 		Table expected = new Table();
 		expected.setState("reserved");
-		t = TableManager.make_reservation(-5, "21:30", 2);
+		t = TableManager.make_reservation(2000, "700:30", 2000);
 		assertEquals(expected.state(), t.state());
 	}
 }
